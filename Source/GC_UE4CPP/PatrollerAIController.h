0dbe506d02c14af26ca977506d17d6e37d03c801
Arthur <tutur716@gmail.com>
2022-01-21 17:10:45 +0100

    base

---
+++ Source/GC_UE4CPP/PatrollerAIController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "PatrollerAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GC_UE4CPP_API APatrollerAIController : public AAIController
+{
+	GENERATED_BODY()
+
+};
