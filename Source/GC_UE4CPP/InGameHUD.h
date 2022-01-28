fdd7112f77c32261e41963f4d9e125f8cf84c3ce
Arthur <tutur716@gmail.com>
2022-01-28 09:50:50 +0100

    Auto stash before merge of "tutur" and "release"

---
+++ Source/GC_UE4CPP/InGameHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "InGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GC_UE4CPP_API UInGameHUD : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
