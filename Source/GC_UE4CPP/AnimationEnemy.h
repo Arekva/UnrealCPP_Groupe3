e7510b2c9ad9cdb2087a53d8bdb18cfa69450884
Arthur <tutur716@gmail.com>
2022-02-10 22:14:47 +0100

    More comments

--- Source/GC_UE4CPP/AnimationEnemy.h
+++ Source/GC_UE4CPP/AnimationEnemy.h
@@ -1,15 +1,13 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
-#include "Animation/AnimInstance.h"
+
 #include "AIEnemy.h"
+
 #include "AnimationEnemy.generated.h"
 
-/**
- * 
- */
+class UAnimNotify;
+
 UCLASS()
 class GC_UE4CPP_API UAnimationEnemy : public UAnimInstance
 {
